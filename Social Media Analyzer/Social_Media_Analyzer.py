8b6fcba5f5b482c3d399ca88934717c485731eb4
Dnisde <dede1020qzz@gmail.com>
2021-11-10 01:44:45 -0500

    Remove redundant annotation and useless code

--- Social Media Analyzer/Social_Media_Analyzer.py
+++ Social Media Analyzer/Social_Media_Analyzer.py
@@ -5,18 +5,20 @@
 import requests
 from flask import Flask, session
 import json
-import tweepy # https://github.com/tweepy/tweepy
+import tweepy
 import pandas as pd
 import math
 import argparse
+
 # Imports the Google Cloud client library
 from google.cloud import language_v1
+client = language_v1.LanguageServiceClient.from_service_account_json(json_path)
+
 #Twitter API credentials
 consumer_key = "Your_Customer_Key"
 consumer_secret = "Your_Customer_Secret"
 json_path = "The_path_of_your_GoogleCloud_Client_Key"
-# Instantiates a client
-client = language_v1.LanguageServiceClient.from_service_account_json(json_path)
+
 
 class Social_Media_Analyzer:
 
@@ -48,9 +50,7 @@ class Social_Media_Analyzer:
         search_words = self.keywords
         api = tweepy.API(self.auth, wait_on_rate_limit=True)
         new_search = search_words + " -filter:retweets"
-        # Send tweets through API access
-        # api.update_status("Look, I'm tweeting from #Python in my #earthanalytics class! @EarthLabCU")
-
+       
         # Search the tweets hit by the keywords of number of #5
         tweets = tweepy.Cursor(api.search_tweets, q=search_words, lang="en", since="2020-01-01").items(10)
         for tweet in tweets:
@@ -133,9 +133,15 @@ class Social_Media_Analyzer:
 
 
 if __name__ == "__main__":
-    web_app = Social_Media_Analyzer(consumer_key, consumer_secret, "Happy")
-    # test_App connection
+    # Utilize the analysis engine to search
+    # Parameter 1: Your comsumer_key,
+    # Parameter 2: Your comsumer_credentials / secret
+    # Parameter 3: Key words that you want to search by the engine, either "brand" or "Buzz word"
+    web_app = Social_Media_Analyzer(consumer_key, consumer_secret, "Disney Movies ratings")
     print(web_app)
     web_app()
+    # Test Case 1 : Module Design for User Story 1: Data Scientist
+    # web_app.search_People_ByKeywords()
+
+    # Test Case 2: Module Design for User Story 2: commercial tenant
     # web_app.search_Bykeywords()
-    web_app.search_People_ByKeywords()
